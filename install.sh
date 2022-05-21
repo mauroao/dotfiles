af3c9e64993059aa89110f72d942482c4eda045f
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2022-05-21 13:06:51 -0300

    improvements

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh  --output $HOME/.git-prompt.sh
 
