0c1d401c540343a4bfb4bf054ca0faaf0a721dc6
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2022-05-23 19:35:03 -0300

    features

--- install.sh
+++ install.sh
@@ -1,16 +1,34 @@
+if [[ $OS_TYPE == "darwin"* ]]; then
+    IS_MAC=true
+else
+    IS_MAC=false
+fi
+
+if [[ $IS_MAC ]]; then
+    echo "OS Type = MAC"
+else
+    echo "OS Type = Linux"
+fi
+
 function create_symbolic_link() {
     rm -rf ~/"$1"
     ln -s ~/.dotfiles/"$1" ~/"$1"
+    echo "created link $1"
 }
 
 function download_file() {
     rm -rf ~/."$1"
-    curl https://raw.githubusercontent.com/git/git/master/contrib/completion/"$1" --output ~/."$1"
+    curl --silent  https://raw.githubusercontent.com/git/git/master/contrib/completion/"$1" --output ~/."$1"
+    echo "downloaded $1"
 }
-         https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-create_symbolic_link ".zshrc"
+
+if [[ $IS_MAC ]]; then
+    create_symbolic_link ".zshrc"
+else
+    download_file "git-completion.bash"
+fi
+
 create_symbolic_link ".vimrc"
 create_symbolic_link ".gitconfig"
 download_file "git-prompt.sh"
-download_file "git-completion.bash"
 
