f4873a505ed183d132233a44230ab02b4eb708f1
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2022-05-23 18:01:41 -0300

    add git completion for ubuntu

--- .bashrc
+++ .bashrc
@@ -2,6 +2,7 @@
 # (from https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh)
 # (from https://scriptim.github.io/bash-prompt-generator/)
 
+source ~/.git-completion.bash
 source ~/.git-prompt.sh
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@ function create_symbolic_link() {
     ln -s ~/.dotfiles/"$1" ~/"$1"
 }
 
+function download_file() {
+    rm -rf ~/."$1"
+    curl https://raw.githubusercontent.com/git/git/master/contrib/completion/"$1" --output ~/."$1"
+}
+         https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 create_symbolic_link ".zshrc"
 create_symbolic_link ".vimrc"
 create_symbolic_link ".gitconfig"
-
-GIT_SCRIPT="git-prompt.sh"
-rm -rf ~/."$GIT_SCRIPT"
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/"$GIT_SCRIPT" --output ~/."$GIT_SCRIPT"
+download_file "git-prompt.sh"
+download_file "git-completion.bash"
 
