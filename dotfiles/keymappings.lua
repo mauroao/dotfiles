3f25f24ead6bb8d0539e698e1676b48f70049159
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2022-06-15 21:50:03 -0300

    modified:   dotfiles/keymappings.lua

--- dotfiles/keymappings.lua
+++ dotfiles/keymappings.lua
@@ -5,5 +5,7 @@ vim.g.mapleader = ' '
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true } )
 vim.api.nvim_set_keymap('n', '<leader>h', '<C-w>h', { noremap = true } )
 vim.api.nvim_set_keymap('n', '<leader>l', '<C-w>l', { noremap = true } )
+vim.api.nvim_set_keymap('n', '<leader>j', '<C-w>j', { noremap = true } )
+vim.api.nvim_set_keymap('n', '<leader>k', '<C-w>k', { noremap = true } )
 vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true } )
 vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true } )
