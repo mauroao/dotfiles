5473c367a4326f8279e09897e140d539bea292a5
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2022-07-09 23:57:58 -0300

    removing comments

--- dotfiles/plugins.lua
+++ dotfiles/plugins.lua
@@ -1,43 +1,17 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'            -- packer manage it self
 
-    -- https://github.com/Mofiqul/vscode.nvim
+    use 'kyazdani42/nvim-web-devicons'      -- used by other plugins
+    use 'nvim-lua/plenary.nvim'             -- used by other plugins
+
     use 'Mofiqul/vscode.nvim'
-    
-    -- https://github.com/nanotee/nvim-lua-guide
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons', -- optional, for file icon
-        },
-        tag = 'nightly' -- optional, updated every week. (see issue #1193)
-   }
-   
-    -- https://github.com/windwp/nvim-autopairs
+    use 'kyazdani42/nvim-tree.lua'
     use 'windwp/nvim-autopairs'
-
-    -- https://github.com/nvim-lualine/lualine.nvim
     use 'nvim-lualine/lualine.nvim'
-
-    -- https://github.com/gpanders/editorconfig.nvim
     use 'gpanders/editorconfig.nvim'
-
-    -- https://github.com/nvim-telescope/telescope.nvim
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }   
-
-    -- https://github.com/romgrk/barbar.nvim
-    use {
-        'romgrk/barbar.nvim',
-        requires = {'kyazdani42/nvim-web-devicons'}
-    }
-    
+    use 'nvim-telescope/telescope.nvim'
+    use 'romgrk/barbar.nvim'
 end)
