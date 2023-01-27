372fb69a4bab3b7e65e878f2c6179259175c4ee3
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2023-01-27 19:58:02 -0300

    indentation

--- dotfiles/nvim/lua/setup.lua
+++ dotfiles/nvim/lua/setup.lua
@@ -3,7 +3,7 @@ vim.cmd([[colorscheme vscode]])
 
 -- Nvim Tree:
 require'nvim-tree'.setup {
-    open_on_setup = true
+  open_on_setup = true
 }
 
 -- Autopairs
@@ -15,38 +15,38 @@ require('lualine').setup()
 -- Telescope
 local actions = require('telescope.actions')
 require('telescope').setup {
-    defaults = {
-        mappings = {
-            n = {
-                ["q"] = actions.close
-            }
-        }
+  defaults = {
+    mappings = {
+      n = {
+        ["q"] = actions.close
+      }
     }
+  }
 }
 
 -- Set barbar's options
 require('bufferline').setup {
-    animation = true,
-    auto_hide = false,
-    tabpages = true,
-    closable = true,
-    clickable = true,
-    --exclude_ft = {'javascript'},
-    --exclude_name = {'package.json'},
-    icons = true,
-    icon_custom_colors = false,
-    icon_separator_active = '▎',
-    icon_separator_inactive = '▎',
-    icon_close_tab = '',
-    icon_close_tab_modified = '●',
-    icon_pinned = '車',
-    insert_at_end = false,
-    insert_at_start = false,
-    maximum_padding = 1,
-    maximum_length = 30,
-    semantic_letters = true,
-    letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
-    no_name_title = nil,
+  animation = true,
+  auto_hide = false,
+  tabpages = true,
+  closable = true,
+  clickable = true,
+  --exclude_ft = {'javascript'},
+  --exclude_name = {'package.json'},
+  icons = true,
+  icon_custom_colors = false,
+  icon_separator_active = '▎',
+  icon_separator_inactive = '▎',
+  icon_close_tab = '',
+  icon_close_tab_modified = '●',
+  icon_pinned = '車',
+  insert_at_end = false,
+  insert_at_start = false,
+  maximum_padding = 1,
+  maximum_length = 30,
+  semantic_letters = true,
+  letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+  no_name_title = nil,
 }
 
 -- Wich key
@@ -57,11 +57,11 @@ local nvim_tree_events = require('nvim-tree.events')
 local bufferline_state = require('bufferline.api')
 
 nvim_tree_events.on_tree_open(function ()
-    bufferline_state.set_offset(31, "File Tree")
+  bufferline_state.set_offset(31, "File Tree")
 end)
 
 nvim_tree_events.on_tree_close(function ()
-    bufferline_state.set_offset(0)
+  bufferline_state.set_offset(0)
 end)
 
 -- Illuminate
@@ -70,18 +70,18 @@ require('illuminate').configure()
 -- Treesitter
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {
-      "python",
-      "javascript",
-      "typescript",
-      "c_sharp",
-      "lua",
-      "html",
-      "css",
-      "json",
-      "bash",
-      "markdown",
-      "yaml"
-    },
+    "python",
+    "javascript",
+    "typescript",
+    "c_sharp",
+    "lua",
+    "html",
+    "css",
+    "json",
+    "bash",
+    "markdown",
+    "yaml"
+  },
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
@@ -91,7 +91,7 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
- -- LSP progress
- require"fidget".setup{}
+-- LSP progress
+require"fidget".setup{}
 
 
