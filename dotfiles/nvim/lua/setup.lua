1c6aa59311a54a1eed64f23474ef0b258b33df82
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2023-02-02 01:00:54 -0300

    fix copy and paste in wsl

--- dotfiles/nvim/lua/setup.lua
+++ dotfiles/nvim/lua/setup.lua
@@ -134,4 +134,17 @@ require'nvim-treesitter.configs'.setup {
 -- LSP progress
 require"fidget".setup{}
 
+-- WSL 2 hack for copy and paste
+
+vim.opt.clipboard = "unnamedplus"
+
+if vim.fn.has('wsl') == 1 then
+  vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+    callback = function()
+      vim.fn.system('clip.exe', vim.fn.getreg('"'))
+    end,
+  })
+end
+
 
