af8e04463e388bafe3cdbefc6f43f6b092bdaa53
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2023-07-17 01:51:16 -0300

    .

--- dotfiles/lvim/config.lua
+++ dotfiles/lvim/config.lua
@@ -1,2 +1,10 @@
 vim.api.nvim_set_keymap('i', 'jj',         '<ESC>',                     { noremap = true } )
 vim.api.nvim_set_keymap('i', 'jk',         '<ESC>',                     { noremap = true } )
+
+lvim.plugins = {
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require"lsp_signature".on_attach({ hint_enable = false }) end,
+  },
+}
